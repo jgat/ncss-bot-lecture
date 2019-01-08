81705b0bdbacd45b8aa40efcc063ec9ca387605f
Jackson Gatenby <jackson.gatenby@gmail.com>
2019-01-08 20:39:44 +1100

    Run my code before committing it

--- slack_handler.py
+++ slack_handler.py
@@ -6,7 +6,7 @@ import re
 @app.route('/slack/ask', methods=['GET', 'POST'])
 def slack_handler():
   question = request.values.get('text', '')
-  match = re.match(r'(where|what|when) (is|are) (the|my)? *(.*)', text, re.IGNORECASE)
+  match = re.match(r'^(where|what|when) (is|are) (the|my)? *(.*?)\??$', question, re.IGNORECASE)
   if match:
-    return f'What is {match.group(4)}?'
+    return f'_What_ {match.group(2)} {match.group(4)}?'
   return "I'm not sure I understand..."
